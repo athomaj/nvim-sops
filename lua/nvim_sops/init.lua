86cee5e34f03febe9ba8663c2a590123f4e9d61b
athomaj <julien@athomas.io>
2024-10-22 09:37:25 +0200

    trying to include public age key

--- lua/nvim_sops/init.lua
+++ lua/nvim_sops/init.lua
@@ -1,35 +1,39 @@
-local utils = require "nvim_sops.utils"
-local commands = require "nvim_sops.commands"
+local utils = require("nvim_sops.utils")
+local commands = require("nvim_sops.commands")
 local M = {}
 
 M.init = function()
-  if not vim.g.nvim_sops_namespace then
-    vim.g.nvim_sops_namespace = vim.api.nvim_create_namespace "nvim_sops"
-  end
+	if not vim.g.nvim_sops_namespace then
+		vim.g.nvim_sops_namespace = vim.api.nvim_create_namespace("nvim_sops")
+	end
 end
 
 M.setup = function(options)
-  if options == nil then
-    options = {}
-  end
-  if options.defaults == nil then
-    options.defaults = {}
-  end
+	if options == nil then
+		options = {}
+	end
+	if options.defaults == nil then
+		options.defaults = {}
+	end
 
-  local o = utils.first_not_nil
+	local o = utils.first_not_nil
 
-  vim.g.nvim_sops_enabled = o(options.enabled, vim.g.nvim_sops_enabled, true)
-  vim.g.nvim_sops_debug = o(options.debug, vim.g.nvim_sops_debug, false)
-  vim.g.nvim_sops_bin_path = o(options.binPath, vim.g.nvim_sops_bin_path, "sops")
+	vim.g.nvim_sops_enabled = o(options.enabled, vim.g.nvim_sops_enabled, true)
+	vim.g.nvim_sops_debug = o(options.debug, vim.g.nvim_sops_debug, false)
+	vim.g.nvim_sops_bin_path = o(options.binPath, vim.g.nvim_sops_bin_path, "sops")
 
-  vim.g.nvim_sops_defaults_aws_profile = o(options.defaults.awsProfile, vim.g.nvim_sops_defaults_aws_profile,
-    os.getenv("AWS_PROFILE"))
-  vim.g.nvim_sops_defaults_age_key_file = o(options.defaults.ageKeyFile, vim.g.nvim_sops_defaults_age_key_file,
-    os.getenv("SOPS_AGE_KEY_FILE"))
-  vim.g.nvim_sops_defaults_gcp_credentials_path = o(options.defaults.gcpCredentialsPath, vim.g.nvim_sops_defaults_gcp_credentials_path,
-    os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-
-  vim.g.__nvim_sops_setup_completed = true
+	vim.g.nvim_sops_defaults_aws_profile =
+		o(options.defaults.awsProfile, vim.g.nvim_sops_defaults_aws_profile, os.getenv("AWS_PROFILE"))
+	vim.g.nvim_sops_defaults_age_key_file =
+		o(options.defaults.ageKeyFile, vim.g.nvim_sops_defaults_age_key_file, os.getenv("SOPS_AGE_KEY_FILE"))
+	vim.g.nvim_sops_defaults_gcp_credentials_path = o(
+		options.defaults.gcpCredentialsPath,
+		vim.g.nvim_sops_defaults_gcp_credentials_path,
+		os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	)
+	vim.g.nvim_sops_defaults_age_public_key =
+		o(options.defaults.pubAgeKey, vim.g.nvim.nvim_sops_defaults_age_public_key)
+	vim.g.__nvim_sops_setup_completed = true
 end
 
 M.commands = commands
--- lua/nvim_sops/sops.lua
+++ lua/nvim_sops/sops.lua
@@ -2,31 +2,33 @@ local debug = require("nvim_sops.utils").debug
 local M = {}
 
 M.get_sops_general_options = function()
-  local awsProfile = vim.g.nvim_sops_defaults_aws_profile
-  local gcpCredentialsPath = vim.g.nvim_sops_defaults_gcp_credentials_path
-  local ageKeyFile = vim.g.nvim_sops_defaults_age_key_file
+	local awsProfile = vim.g.nvim_sops_defaults_aws_profile
+	local gcpCredentialsPath = vim.g.nvim_sops_defaults_gcp_credentials_path
+	local ageKeyFile = vim.g.nvim_sops_defaults_age_key_file
+	local sopsGeneralEnvVars = {}
 
-  local sopsGeneralEnvVars = {}
+	if awsProfile then
+		sopsGeneralEnvVars.AWS_PROFILE = awsProfile
+	end
 
-  if awsProfile then
-    sopsGeneralEnvVars.AWS_PROFILE = awsProfile
-  end
+	if gcpCredentialsPath then
+		sopsGeneralEnvVars.GOOGLE_APPLICATION_CREDENTIALS = gcpCredentialsPath
+	end
 
-  if gcpCredentialsPath then
-    sopsGeneralEnvVars.GOOGLE_APPLICATION_CREDENTIALS = gcpCredentialsPath
-  end
+	if ageKeyFile then
+		sopsGeneralEnvVars.SOPS_AGE_KEY_FILE = ageKeyFile
+	end
 
-  if ageKeyFile then
-    sopsGeneralEnvVars.SOPS_AGE_KEY_FILE = ageKeyFile
-  end
+	for key, value in pairs(sopsGeneralEnvVars) do
+		debug("sops option: " .. key .. " = " .. value)
+	end
 
-  for key, value in pairs(sopsGeneralEnvVars) do
-    debug('sops option: ' .. key .. ' = ' .. value)
-  end
-
-  return {
-    sopsGeneralEnvVars = sopsGeneralEnvVars
-  }
+	return {
+		sopsGeneralEnvVars = sopsGeneralEnvVars,
+	}
 end
-
+M.get_sops_age_pub_key = function()
+	local publicKey = vim.g.nvim_sops_defaults_age_public_key
+	return { publicKey }
+end
 return M
